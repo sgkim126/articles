80a11cec7c1ededfa4c5ab160864d7018b35e59a
Seulgi Kim <dev@seulgi.kim>
2015-01-10 02:24:00 +0900

    Add exhibition.*

---
+++ diary/exhibition.js
@@ -0,0 +1,2 @@
+document.write('<h2 style="border-bottom-width: 0px;">전시회</h2><ul><li><a href="http://diary.seulgi.kim/2014/03/blog-post.html" target="_blank">스팀 펑크 아트전</a></li><li><a href="http://diary.seulgi.kim/2014/07/orsay.html" target="_blank">오르세미술관전</a></li></ul>');
+
