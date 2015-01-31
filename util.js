a71b0323a6bb502243a81e1458568f474f1a429f
Seulgi Kim <dev@seulgi.kim>
2015-01-31 11:44:51 +0900

    Fix line length.

--- util.js
+++ util.js
@@ -10,9 +10,7 @@ var li = function(href, title) {
 
 var write = function(tag) {
   var result_text = tag.prop('innerHTML');
-  var write = 'document.write(\'' +
-	  result_text.split('\'').join('\\\'') +
-	  '\');';
+  var write = 'document.write(\'' + result_text.split('\'').join('\\\'') + '\');';
   console.log(write);
   document.write('<xmp>' + write + '</xmp>' + '<br>' + result_text);
 }
